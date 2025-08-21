8f321be8995c3b319acd6b9136af43dc8fb965bc
Harsh Yadav <141502435+Harsh-krishna11@users.noreply.github.com>
2025-08-21 12:59:14 +0530

    Update server.js

--- faceDetection-main/face-auth-main/server.js
+++ faceDetection-main/face-auth-main/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://facedetection-main-frontend.onrender.com,
+    origin: "https://facedetection-main-frontend.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
